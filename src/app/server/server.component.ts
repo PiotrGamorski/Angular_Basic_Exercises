071f1e7cf0185759b037808fe5c3dbf8e2067b19
Piotr Gamorski <pgamorski@wp.pl>
2021-09-08 21:17:21 +0200

    Observable exercises

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
-import { Timestamp } from "rxjs";
+import { fromEvent, Observer } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-server',
@@ -13,6 +14,29 @@ export class ServerComponent {
   showDetails: boolean = false;
   clicksArray: number[] = [];
   lessThenFive: boolean = true;
+  
+  myOberver: Observer<string> = {
+    next: function(value: string){
+      console.log(value)
+    },
+    error: function(error: Error){
+      console.log(error);
+    },
+    complete: function() {
+      console.log('completed');
+    }
+  }
+
+  //create observable that emits click events
+  source = fromEvent<MouseEvent>(document, 'click');
+  //map to string with given event timestamp
+  example = this.source.pipe(map(event => `Event time: ${event.timeStamp}`));
+  example2 = this.source.pipe(filter(data => data.clientX > 300 && data.clientY > 300));
+
+  //output
+  // subscription = this.example.subscribe((value) => console.log(value));
+  subscription = this.example.subscribe(this.myOberver);
+  subscription2 = this.example2.subscribe((data) => console.log(data));
 
   constructor(){
     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
